ed2c946f1cb08535ffa553d7e8310776853bb1f2
刘 家真 <jiazhen.liu@vistel.cn>
2022-04-28 11:21:34 +0800

    flow control

--- src/components/Drawer/Drawer_cell.vue
+++ src/components/Drawer/Drawer_cell.vue
@@ -7,6 +7,131 @@
         :body-style="{ paddingBottom: '80px' }"
         @close="onClose"
     >
+      <a-form :form="form" layout="vertical" hide-required-mark>
+        <a-row :gutter="16">
+          <a-col :span="12">
+            <a-form-item label="参比电极输入阻抗">
+              <a-input
+                  v-decorator="[
+                  'name',
+                  {
+                    rules: [{ required: true, message: 'Please enter user name' }],
+                  },
+                ]"
+                  placeholder="请输入阻抗"
+              />
+            </a-form-item>
+          </a-col>
+          <a-col :span="12">
+            <a-form-item label="扫描速度">
+              <a-input
+                  v-decorator="[
+                  'name',
+                  {
+                    rules: [{ required: true, message: 'Please enter user name' }],
+                  },
+                ]"
+                  placeholder="请输入扫描速度"
+              />
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row :gutter="16">
+          <a-col :span="12">
+            <a-form-item label="Owner">
+              <a-select
+                  v-decorator="[
+                  'owner',
+                  {
+                    rules: [{ required: true, message: 'Please select an owner' }],
+                  },
+                ]"
+                  placeholder="测量电流范围"
+              >
+                <a-select-option value="xiao">
+                  ±0.025 V
+                </a-select-option>
+                <a-select-option value="mao">
+                  ±0.1 V
+                </a-select-option>
+                <a-select-option value="xiao1">
+                  ±0.25 V
+                </a-select-option>
+                <a-select-option value="xiao2">
+                  ±1 V
+                </a-select-option>
+              </a-select>
+            </a-form-item>
+          </a-col>
+          <a-col :span="12">
+            <a-form-item label="CA的最小采样间隔">
+              <a-input
+                  v-decorator="[
+                  'name',
+                  {
+                    rules: [{ required: true, message: 'Please enter user name' }],
+                  },
+                ]"
+                  placeholder="请输入采样间隔"
+              />
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <!--        <a-row :gutter="16">-->
+        <!--          <a-col :span="12">-->
+        <!--            <a-form-item label="Approver">-->
+        <!--              <a-select-->
+        <!--                  v-decorator="[-->
+        <!--                  'approver',-->
+        <!--                  {-->
+        <!--                    rules: [{ required: true, message: 'Please choose the approver' }],-->
+        <!--                  },-->
+        <!--                ]"-->
+        <!--                  placeholder="Please choose the approver"-->
+        <!--              >-->
+        <!--                <a-select-option value="jack">-->
+        <!--                  Jack Ma-->
+        <!--                </a-select-option>-->
+        <!--                <a-select-option value="tom">-->
+        <!--                  Tom Liu-->
+        <!--                </a-select-option>-->
+        <!--              </a-select>-->
+        <!--            </a-form-item>-->
+        <!--          </a-col>-->
+        <!--          <a-col :span="12">-->
+        <!--            <a-form-item label="DateTime">-->
+        <!--              <a-date-picker-->
+        <!--                  v-decorator="[-->
+        <!--                  'dateTime',-->
+        <!--                  {-->
+        <!--                    rules: [{ required: true, message: 'Please choose the dateTime' }],-->
+        <!--                  },-->
+        <!--                ]"-->
+        <!--                  style="width: 100%"-->
+        <!--                  :get-popup-container="trigger => trigger.parentNode"-->
+        <!--              />-->
+        <!--            </a-form-item>-->
+        <!--          </a-col>-->
+        <!--        </a-row>-->
+        <!--        <a-row :gutter="16">-->
+        <!--          <a-col :span="24">-->
+        <!--            <a-form-item label="Description">-->
+        <!--              <a-textarea-->
+        <!--                  v-decorator="[-->
+        <!--                  'description',-->
+        <!--                  {-->
+        <!--                    rules: [{ required: true, message: 'Please enter url description' }],-->
+        <!--                  },-->
+        <!--                ]"-->
+        <!--                  :rows="4"-->
+        <!--                  placeholder="please enter url description"-->
+        <!--              />-->
+        <!--            </a-form-item>-->
+        <!--          </a-col>-->
+        <!--        </a-row>-->
+
+
+      </a-form>
       <div
           :style="{
           position: 'absolute',
--- src/components/Drawer/Drawer_mico.vue
+++ src/components/Drawer/Drawer_mico.vue
@@ -10,7 +10,7 @@
       <a-form :form="form" layout="vertical" hide-required-mark>
         <a-row :gutter="16">
           <a-col :span="12">
-            <a-form-item label="Name">
+            <a-form-item label="参比电极输入阻抗">
               <a-input
                   v-decorator="[
                   'name',
@@ -18,23 +18,20 @@
                     rules: [{ required: true, message: 'Please enter user name' }],
                   },
                 ]"
-                  placeholder="Please enter user name"
+                  placeholder="请输入阻抗"
               />
             </a-form-item>
           </a-col>
           <a-col :span="12">
-            <a-form-item label="Url">
+            <a-form-item label="扫描速度">
               <a-input
                   v-decorator="[
-                  'url',
+                  'name',
                   {
-                    rules: [{ required: true, message: 'please enter url' }],
+                    rules: [{ required: true, message: 'Please enter user name' }],
                   },
                 ]"
-                  style="width: 100%"
-                  addon-before="http://"
-                  addon-after=".com"
-                  placeholder="please enter url"
+                  placeholder="请输入扫描速度"
               />
             </a-form-item>
           </a-col>
@@ -49,90 +46,89 @@
                     rules: [{ required: true, message: 'Please select an owner' }],
                   },
                 ]"
-                  placeholder="Please a-s an owner"
+                  placeholder="测量电流范围"
               >
                 <a-select-option value="xiao">
-                  Xiaoxiao Fu
+                  ±0.025 V
                 </a-select-option>
                 <a-select-option value="mao">
-                  Maomao Zhou
+                  ±0.1 V
+                </a-select-option>
+                <a-select-option value="xiao1">
+                  ±0.25 V
+                </a-select-option>
+                <a-select-option value="xiao2">
+                  ±1 V
                 </a-select-option>
               </a-select>
             </a-form-item>
           </a-col>
           <a-col :span="12">
-            <a-form-item label="Type">
-              <a-select
+            <a-form-item label="CA的最小采样间隔">
+              <a-input
                   v-decorator="[
-                  'type',
+                  'name',
                   {
-                    rules: [{ required: true, message: 'Please choose the type' }],
+                    rules: [{ required: true, message: 'Please enter user name' }],
                   },
                 ]"
-                  placeholder="Please choose the type"
-              >
-                <a-select-option value="private">
-                  Private
-                </a-select-option>
-                <a-select-option value="public">
-                  Public
-                </a-select-option>
-              </a-select>
-            </a-form-item>
-          </a-col>
-        </a-row>
-        <a-row :gutter="16">
-          <a-col :span="12">
-            <a-form-item label="Approver">
-              <a-select
-                  v-decorator="[
-                  'approver',
-                  {
-                    rules: [{ required: true, message: 'Please choose the approver' }],
-                  },
-                ]"
-                  placeholder="Please choose the approver"
-              >
-                <a-select-option value="jack">
-                  Jack Ma
-                </a-select-option>
-                <a-select-option value="tom">
-                  Tom Liu
-                </a-select-option>
-              </a-select>
-            </a-form-item>
-          </a-col>
-          <a-col :span="12">
-            <a-form-item label="DateTime">
-              <a-date-picker
-                  v-decorator="[
-                  'dateTime',
-                  {
-                    rules: [{ required: true, message: 'Please choose the dateTime' }],
-                  },
-                ]"
-                  style="width: 100%"
-                  :get-popup-container="trigger => trigger.parentNode"
+                  placeholder="请输入采样间隔"
               />
             </a-form-item>
           </a-col>
         </a-row>
-        <a-row :gutter="16">
-          <a-col :span="24">
-            <a-form-item label="Description">
-              <a-textarea
-                  v-decorator="[
-                  'description',
-                  {
-                    rules: [{ required: true, message: 'Please enter url description' }],
-                  },
-                ]"
-                  :rows="4"
-                  placeholder="please enter url description"
-              />
-            </a-form-item>
-          </a-col>
-        </a-row>
+<!--        <a-row :gutter="16">-->
+<!--          <a-col :span="12">-->
+<!--            <a-form-item label="Approver">-->
+<!--              <a-select-->
+<!--                  v-decorator="[-->
+<!--                  'approver',-->
+<!--                  {-->
+<!--                    rules: [{ required: true, message: 'Please choose the approver' }],-->
+<!--                  },-->
+<!--                ]"-->
+<!--                  placeholder="Please choose the approver"-->
+<!--              >-->
+<!--                <a-select-option value="jack">-->
+<!--                  Jack Ma-->
+<!--                </a-select-option>-->
+<!--                <a-select-option value="tom">-->
+<!--                  Tom Liu-->
+<!--                </a-select-option>-->
+<!--              </a-select>-->
+<!--            </a-form-item>-->
+<!--          </a-col>-->
+<!--          <a-col :span="12">-->
+<!--            <a-form-item label="DateTime">-->
+<!--              <a-date-picker-->
+<!--                  v-decorator="[-->
+<!--                  'dateTime',-->
+<!--                  {-->
+<!--                    rules: [{ required: true, message: 'Please choose the dateTime' }],-->
+<!--                  },-->
+<!--                ]"-->
+<!--                  style="width: 100%"-->
+<!--                  :get-popup-container="trigger => trigger.parentNode"-->
+<!--              />-->
+<!--            </a-form-item>-->
+<!--          </a-col>-->
+<!--        </a-row>-->
+<!--        <a-row :gutter="16">-->
+<!--          <a-col :span="24">-->
+<!--            <a-form-item label="Description">-->
+<!--              <a-textarea-->
+<!--                  v-decorator="[-->
+<!--                  'description',-->
+<!--                  {-->
+<!--                    rules: [{ required: true, message: 'Please enter url description' }],-->
+<!--                  },-->
+<!--                ]"-->
+<!--                  :rows="4"-->
+<!--                  placeholder="please enter url description"-->
+<!--              />-->
+<!--            </a-form-item>-->
+<!--          </a-col>-->
+<!--        </a-row>-->
 
 
       </a-form>
